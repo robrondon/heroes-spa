2710e11379a1ad4628eb64cb11c51dfed5cb07bd
Robert Rondon <robrondon11@gmail.com>
2025-04-12 08:02:41 -0300

    First Router and folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './styles.css'
 import { HeroesApp } from './HeroesApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </StrictMode>,
 )
