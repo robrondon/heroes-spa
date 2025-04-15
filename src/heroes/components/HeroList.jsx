a915af1236fc68b06c32275418df2bd35e32be33
Robert Rondon <robrondon11@gmail.com>
2025-04-15 20:06:54 -0300

    Moving assets folder to root and creating HeroCard component WIP

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { HeroCard } from './'
 import { getHeroesByPublisher } from '../helpers'
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher)
   return (
-    <ul>
-      {heroes.map(hero => <li key={hero.id}>{hero.superhero}</li>)}
-    </ul>
+    <div className='row row-cols-1 row-cols-md-3 g-3 '>
+      {heroes.map(hero => <HeroCard key={hero.id} {...hero} />)}
+    </div>
   )
 }
