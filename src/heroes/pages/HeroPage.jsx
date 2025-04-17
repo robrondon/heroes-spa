39f3fb41bb48cd55624a5e90ab892032354d36d6
Robert Rondon <robrondon11@gmail.com>
2025-04-17 12:58:25 -0300

    new helper getHeroById and reading params in HeroPage component

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,11 @@
+import { Navigate, useParams } from "react-router"
+import { getHeroById } from "../helpers"
+
 export const HeroPage = () => {
+  const { id } = useParams()
+  const hero = getHeroById(id)
+  if (!hero) return <Navigate to='/marvel' />
+  console.log(hero)
   return (
     <h1>HeroPage</h1>
   )
