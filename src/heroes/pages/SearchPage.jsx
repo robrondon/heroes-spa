53e92402e035744993e13cb77960d70c99783fc3
Robert Rondon <robrondon11@gmail.com>
2025-04-17 15:55:32 -0300

    adding useForm hook and queryString library

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,24 @@
 // import { HeroCard } from '../components'
 
+import { useLocation, useNavigate } from "react-router"
+import queryString from 'query-string'
+import { useForm } from "../../hooks"
+
 export const SearchPage = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { searchText, onInputChange, onResetForm } = useForm({ searchText: '' })
+  const { q = '' } = queryString.parse(location.search)
+
+  const onSearch = (e) => {
+    e.preventDefault()
+    const trimmedText = searchText.toLowerCase().trim()
+
+    if (trimmedText.length <= 1) return
+
+    navigate(`?q=${trimmedText}`)
+    onResetForm()
+  }
   return (
     <>
       <h1>Search</h1>
@@ -9,13 +27,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSearch}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-1">Search</button>
           </form>
@@ -27,7 +47,7 @@ export const SearchPage = () => {
             Search a hero
           </div>
           <div className="alert alert-danger">
-            Not hero with <b>ABC</b>
+            Not hero with <b>{q}</b>
           </div>
           {/* <HeroCard {...hero} /> */}
         </div>
